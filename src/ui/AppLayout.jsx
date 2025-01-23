ad59896ea04f9139d679026ad261a98b71843ff7
Ad12000 <adityavikramsingh04012@gmail.com>
2025-01-23 21:27:27 +0530

    .

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,7 +10,7 @@ export default function AppLayout() {
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
-
+ 
       <Header />
       <div className="overflow-scroll">
         <main className="mx-auto my-8 max-w-6xl overflow-hidden">
